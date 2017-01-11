ea78eb1963046e9abb2723e9687878593f22ad30
mdziub <mackowiecki@o2.pl>
2017-01-11 20:34:43 +0100

    addcategory added

--- src/main/java/Modele/Service/ModelsService.java
+++ src/main/java/Modele/Service/ModelsService.java
@@ -1,7 +1,39 @@
 package Modele.Service;
 
-/**
- * Created by Dziuba on 10.01.2017.
- */
-public class ModelsService {
+import java.util.ArrayList;
+import java.util.List;
+
+import Modele.Entity.Category;
+import Modele.Entity.Model;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Component
+@Transactional
+class ModelsService implements IModelsManager {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    @Override
+    public Long addCategory(Category category) {
+        category.setId(null);
+        return (Long) sessionFactory.getCurrentSession()
+                .save(category);
+    }
+
+
+
 }
