dfffb75dfa68f150b31d35ba01706f4f5b0f1fee
mdziub <mackowiecki@o2.pl>
2017-01-14 13:12:59 +0100

    Update ModelsServiceTest.java
    tests end

--- src/test/java/Modele/Service/ModelsServiceTest.java
+++ src/test/java/Modele/Service/ModelsServiceTest.java
@@ -156,27 +156,106 @@ public class ModelsServiceTest {
     public void findByProd() throws Exception {
 
         Model m2=new Model();
-        m2.setNazwaMod("asdasdsad");
+        m2.setNazwaMod("Nazwunia");
         m2.setProducent(randomString);
-        m2.setProgram("sadsda");
+        m2.setProgram("3ds");
         modelsManager.addModel(m2);
         assertEquals(randomString,modelsManager
                                     .findByProd(m2.getProducent())
                                         .getProducent());
     }
 
-    @Test
+     @Test
     public void deleteModel() throws Exception {
+        Model m1=new Model();
+        m1.setNazwaMod("biurko");
+        m1.setProducent(randomString);
+        m1.setProgram("3ds");
 
+        Long idM= modelsManager.addModel(m1);
+        setUp();
+        Model m2=new Model();
+        m2.setNazwaMod("lawa");
+        m2.setProducent(randomString);
+        m2.setProgram("3ds");
+        modelsManager.addModel(m2);
+        Long idM2=modelsManager.addModel(m2);
+        //usuwanie modelu
+        modelsManager.deleteModel(m1);
+        boolean ifExists=false;
+        try {
+            assertFalse(modelsManager.findByIdModel(idM).getProducent() != randomString);
+        }catch (NullPointerException e)
+        {
+            ifExists=true;
+        }
+
+        assertTrue(ifExists);
+        //sprawdzamy czy nie zosatly usuniete inne rekordy
+        assertTrue(modelsManager.findByIdModel(idM2).getProducent()==randomString);
     }
 
     @Test
     public void getAllCategoryModel() throws Exception {
+        Category puzzle=new Category();
+        puzzle.setNazwaKat(randomString);
+        puzzle.setNumer(1234);
+        puzzle.setOpis("salon");
+        Long id=modelsManager.addCategory(puzzle);
+        Model m1=new Model();
+        m1.setNazwaMod("komoa");
+        m1.setProducent(randomString);
+        m1.setProgram("3ds");
+        m1.setCategory(puzzle);
+        setUp();
+        Model m2=new Model();
+        m2.setNazwaMod("szafa");
+        m2.setProducent(randomString);
+        m2.setProgram("3ds");
+        m2.setCategory(puzzle);
+        setUp();
+        Model m3=new Model();
+        m3.setNazwaMod("lozko");
+        m3.setProducent(randomString);
+        m3.setProgram("3ds");
 
+        modelsManager.addModel(m1);
+        modelsManager.addModel(m2);
+        Long idM3=modelsManager.addModel(m3);
+        List<Model> models=modelsManager.getAllCategoryModel(id);
+        assertTrue(models.size()==2);
+        //test sprawdzajacy czy przypisuje tylko te odele ktore ma przypisac
+        boolean ifExists=false;
+        try {
+
+            assertTrue(modelsManager
+                    .findByIdModel(idM3)
+                    .getCategory()
+                    .getId() == id);
+        }
+        catch (Exception e){
+            ifExists=true;
+
+        }
+
+        assertTrue(ifExists);
     }
-
     @Test
     public void addCategoryToModel() throws Exception {
-
+        Category puzzle=new Category();
+        puzzle.setNazwaKat(randomString);
+        puzzle.setNumer(1234);
+        puzzle.setOpis("kuchnia");
+        Long idC=modelsManager.addCategory(puzzle);
+        Model m1=new Model();
+        m1.setNazwaMod("piekarnik");
+        m1.setProducent(randomString);
+        m1.setProgram("3ds");
+        Long idM=modelsManager.addModel(m1);
+        modelsManager.addCategoryToModel(idC,idM);
+        List<Model> models=modelsManager.getAllCategoryModel(idC);
+        assertTrue(models.size()!=0);
+        assertEquals(idM,models.get(0).getId());
     }
+
 }
